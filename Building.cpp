c4eafa85e6bf2d5ef3e78819f8d58fd59d70d966
Paul <pjohnsonuni@gmail.com>
2013-11-10 06:28:15 +0000

    Globals unittype enum updated. Building unitType temp removed

--- Building.cpp
+++ Building.cpp
@@ -7,14 +7,37 @@ Building::Building(BuildingType type, Side side)
 	buildingType = type;
 }
 
+Action Building::getActions(Side turn)
+{
+	//If selecting allied building
+	if (side == turn)
+	{
+		return CREATE_UNIT;
+	}
+	else
+	{
+		return NONE;
+	}
+
+}
+
 Action Building::getActions(Side turn, UnitType unitType)
 {
+	//If the unit is allied to the building
 	if (side == turn)
 	{
 		return NONE;
 	}
 	else
 	{
+		//if (unitType == INFANTRY)
+		//{
+		//	return CAPTURE;
+		//}
+		//else
+		//{
+		//	return NONE;
+		//}
 	}
 }
 
