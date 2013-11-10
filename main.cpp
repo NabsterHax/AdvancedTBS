bc74c00af813f60288d6a1397ad82b1bcb22d20e
NabsterHax <nabsterhax@gmail.com>
2013-11-10 03:24:39 +0000

    Fixing git crap

--- main.cpp
+++ main.cpp
@@ -15,13 +15,8 @@
 #include <allegro5/allegro_acodec.h>
 
 #include "Globals.h"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 9b869afecd7a095968db3db71062a1ed10c3bedb
+
 #include <vector>
->>>>>>> 9b869afecd7a095968db3db71062a1ed10c3bedb
 
 void Update();
 void Draw();
